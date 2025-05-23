cd175e4605edb7d77e19498534a9af70757e214c
cyzsh <rykxue.0fx7@gmail.com>
2025-05-23 09:18:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ffslikes', {
+mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
